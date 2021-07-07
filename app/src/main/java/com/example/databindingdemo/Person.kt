49972b76e0a70c5cfb69f43e0e283426daf8127f
ChaiZhiYing <shella_4848@hotmail.com>
2021-07-07 14:16:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/databindingdemo/Person.kt
@@ -0,0 +1,9 @@
+package com.example.databindingdemo;
+
+data class Person (
+    var name: String = "",
+    var IC: String= "",
+    var email: String= "",
+    var address: String= ""
+        )
+
